4ead093140ca887a2cb5540c5acbaf3322bb68c6
John <japperales@gmial.com>
2020-01-28 19:44:47 -0500

    Checkpoint for Cross domain error solutions

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +14,8 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -26,10 +28,46 @@ import {
 import LinearGradient from 'react-native-linear-gradient';
 
 const App: () => React$Node = () => {
-  return (
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginError, setLoginError] = useState();
+    const [jwt, setJwt] = useState();
+
+
+    const attemptLogin = (username, password) => {
+        fetch('https://localhost:5001/api/Puma/PostLogin', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        }).then((response => {
+            const responseObject = JSON.parse(response);
+            if (typeof responseObject.Jwt === undefined) {
+                setLoginError("Login Failed. Please Try Again")
+            } else {
+                setJwt(responseObject.Jwt);
+                setLoginError("");
+
+            }
+           //add navigation here
+        })).catch(function(error) {
+            console.log('There has been a problem with your fetch operation: ' + error.message);
+            // ADD THIS THROW error
+            return error;
+        });
+    };
+
+
+    return (
 
       <LinearGradient
-          colors={["#0575E6", "#021B79"]}
+          colors={["#2a5298", "#1e3c72"]}
           style={{
             position: 'absolute',
             left: 0,
@@ -37,51 +75,90 @@ const App: () => React$Node = () => {
             bottom: 0,
             height: "100%",
           }}>
-        <Text style={{fontFamily: "Lato-BlackItalic", fontSize: 36}}>
-          yay
+
+          <View style={{flex: 1, alignItems: "center"}}>
+
+        <Text style={{fontFamily: "Lato-Regular", fontSize: 64, color: "white"}}>
+          Welcome
         </Text>
+
+        <Text style={{fontFamily: "Lato-Light", fontSize: 32, color: "white"}}>
+          To Accutech Puma
+        </Text>
+
+          </View>
+
+
+              <View style={{flex: 2}}>
+        <Text style= {{textAlign: "center", color: "white", fontSize: 24, fontFamily: "Lato-Medium"}}>Please Log In</Text>
+
+        <TextInput style = {styles.input}
+                   underlineColorAndroid = "transparent"
+                   placeholder = "Username"
+                   placeholderTextColor = "#9a73ef"
+                   autoCapitalize = "none"
+                   backgroundColor = "white"
+                   padding={12}
+                   onChangeText={text => setUsername(text)}
+                   />
+<Text> {username} </Text>
+        <TextInput style = {styles.input}
+                   underlineColorAndroid = "transparent"
+                   placeholder = "Password"
+                   placeholderTextColor = "#9a73ef"
+                   autoCapitalize = "none"
+                   backgroundColor = "white"
+                   padding={12}
+                   secureTextEntry={true}
+                   onChangeText={text => setPassword(text)}
+                   />
+<Text>
+    {password}
+</Text>
+                  <Text>
+                      {jwt}
+                  </Text>
+
+                   <View style= {{flex: 1, alignItems: "center"}}>
+                       <Text style={{textAlign: "center", color: "red", fontSize: 14, fontFamily: "Lato-Medium"}}>{loginError}</Text>
+        <TouchableOpacity style= {{backgroundColor: "green", width: "80%", margin: 30, }} onSubmitEditing={attemptLogin(username, password)}>
+            <Text style={{textAlign: "center", color: "white", fontSize: 30, fontFamily: "Lato-Medium"}}>
+                Sign In
+            </Text>
+        </TouchableOpacity>
+                   </View>
+          </View>
+
       </LinearGradient>
 
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+
+  container: {
+    paddingTop: 23,
+      alignItems: 'center'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1,
+      fontFamily: "Lato-Regular"
   },
-  body: {
-    backgroundColor: Colors.white,
+  submitButton: {
+    backgroundColor: '#7a42f4',
+    padding: 10,
+    margin: 15,
+    height: 40,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  submitButtonText: {
+    color: 'white'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    text: {
+
+    }
 });
 
 export default App;
